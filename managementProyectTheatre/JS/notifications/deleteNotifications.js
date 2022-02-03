af763a9d2a2445770c66ba6b30d40a65f50f4921
Ferseo <90406074+Ferseo@users.noreply.github.com>
2022-02-03 19:13:00 +0100

    repair notifications ok

--- managementProyectTheatre/JS/notifications/deleteNotifications.js
+++ managementProyectTheatre/JS/notifications/deleteNotifications.js
@@ -1,22 +1,20 @@
 //Petición ajax que al pulsar el boton de eliminar en el panel de notificaciones, redirige
 //archivo controlador para que haga la consulta a la bd
-$(document).ready(function() {
-    $('.deleteMaterial').on('click', function(e) {
-        e.preventDefault();
-        var parent = $(this).parent().attr('id');
-        var count = $(this).attr('id');
-        console.log(parent);
-        console.log(count);
 
-        $.ajax({
-            type: "POST",
-            url: "../../controller/deleteNotification.php",
-            data: {
-                count
-            },
-            success: function(response) {
-                window.location.reload();
-            }
-        });
-    });
-});
+function deleteNotifications(){
+    // var parent = $(this).parent().attr('id');
+            var count = $(".deleteNotifications").attr('id');
+            console.log(parent);
+            console.log(count);
+    
+            $.ajax({
+                type: "POST",
+                url: "../../controller/deleteNotification.php",
+                data: {
+                    count
+                },
+                success: function(response) {
+                    window.location.reload();
+                }
+            });
+}
