96b8d4d200854bdbc39f660be55447ea8f707944
Ferseo <90406074+Ferseo@users.noreply.github.com>
2022-03-08 17:02:57 +0100

    register new user modified
    En este paso se modifica la opcion de registrar un nuevo usuario, se elimina el menu de setting y se centran los campos de entrada.

--- managementProyectTheatre/View/registerUser/registerNewUser.php
+++ managementProyectTheatre/View/registerUser/registerNewUser.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,6 +17,7 @@
         width: 99%;
     }
 </style>
+
 <body class="body">
     <div class="row">
         <div class="col-12">
@@ -25,67 +27,73 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-2">
+        <!-- <div class="col-2">
             <ul class="modal-content" style="font-weight: bold; margin-left:10%;margin-top:2%;">
-                <li id="fatherList"><a class="dropdown-item" href="#">Mi perfil</a></li>
-                <li><a class="dropdown-item" href="#">Ajustes de audio</a></li>
-                <li><a class="dropdown-item" href="#">Sincr. Diputación</a></li>
-                <li><a class="dropdown-item" href="#">Ajustes de Calendario</a></li>
-                <li><a class="dropdown-item" href="#">Notificaciones</a></li>
+            <li id="fatherList"><a class="dropdown-item" onclick="miPerfil()">Mi perfil</a></li>
+            <li><a class="dropdown-item" onclick="sincroDipu()">Sincr. Diputación</a></li>
+            <li><a class="dropdown-item" href="#">Ajustes de audio</a></li>
+            <li><a class="dropdown-item" href="#">Ajustes de Calendario</a></li>
+            <li><a class="dropdown-item" href="#">Notificaciones</a></li>
             </ul>
-        </div>
-        <div class="col-10">
-            <div class="container modal-content" style=" width:80%;">
-            <h3>Registro de usuario</h3>
-                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">DNI</span>
+        </div> -->
+        <div class="col">
+            <div class="container modal-content" style=" width:80%; text-align:center;">
+                <h3>Registro de usuario</h3>
+                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%; margin-left:10%;">
+                    <span class="input-group-text" id="inputGroup-sizing-sm" style="width: 15%;">DNI</span>
                     <input type="text" class="form-control" id="dni" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style="width:40%">
                 </div>
 
-                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Nombre</span>
+                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;margin-left:10%;">
+                    <span class="input-group-text" id="inputGroup-sizing-sm" style="width: 15%;">Nombre</span>
                     <input type="text" class="form-control" id="nombre" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style="width:40%">
                 </div>
 
-                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Apellidos</span>
+                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;margin-left:10%;">
+                    <span class="input-group-text" id="inputGroup-sizing-sm" style="width: 15%;">Apellidos</span>
                     <input type="text" class="form-control" id="apellidos" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style="width:40%">
                 </div>
 
-                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Alias</span>
+                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;margin-left:10%;">
+                    <span class="input-group-text" id="inputGroup-sizing-sm" style="width: 15%;">Alias</span>
                     <input type="text" class="form-control" id="user" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style="width:40%">
                 </div>
 
-                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Contraseña</span>
+                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;margin-left:10%;">
+                    <span class="input-group-text" id="inputGroup-sizing-sm" style="width: 15%;">Contraseña</span>
                     <input type="password" class="form-control" id="pass" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style="width:40%">
                 </div>
 
-                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Puesto</span>
+                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;margin-left:10%;">
+                    <span class="input-group-text" id="inputGroup-sizing-sm" style="width: 15%;">Puesto</span>
                     <input type="text" class="form-control" id="categoria" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style="width:40%">
                 </div>
 
-                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Año de ingreso</span>
+                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;margin-left:10%;">
+                    <span class="input-group-text" id="inputGroup-sizing-sm" style="width: 15%;">Año de ingreso</span>
                     <input type="text" class="form-control" id="anio_ingreso" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style="width:40%">
                 </div>
 
-                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Direccion</span>
+                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;margin-left:10%;">
+                    <span class="input-group-text" id="inputGroup-sizing-sm" style="width: 15%;">Direccion</span>
                     <input type="text" class="form-control" id="direccion" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style="width:40%">
                 </div>
 
-                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Teléfono</span>
+                <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;margin-left:10%;">
+                    <span class="input-group-text" id="inputGroup-sizing-sm" style="width: 15%;">Teléfono</span>
                     <input type="text" class="form-control" id="telefono" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style="width:40%">
 
                 </div>
 
-                    <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
-                    <button type="submit" onclick="registerNewUser()" class="btn btn-secondary">Registrar</button>
+                <div class="row">
+                    <div class="col-5"></div>
+                    <div class="col-5"></div>
+                    <div class="col-2">
+                        <div class="input-group input-group-sm mb-3" style="padding:1%; width:80%;">
+                            <button type="submit" onclick="registerNewUser()" class="btn btn-secondary">Registrar</button>
+                        </div>
                     </div>
+                </div>
             </div>
         </div>
 
