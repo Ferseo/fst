f7b88e146123262f905a20f64da7879adbee74b0
Fer Gastby <90406074+Ferseo@users.noreply.github.com>
2022-05-16 18:58:10 +0200

    Update indexBoss.php

--- managementProyectTheatre/View/indexPages/indexBoss.php
+++ managementProyectTheatre/View/indexPages/indexBoss.php
@@ -15,6 +15,58 @@
         background-repeat: no-repeat;
         background-size: auto;
     }
+     @media only screen and (max-width: 600px){
+    h1{
+        display: none;
+    }
+    .global_row{
+        flex-direction: column;
+    }
+    .spanAdd{
+        width:100% !important;
+    }
+    .spanLend{
+        width:100% !important;
+        height: 50% !important
+    }
+    .spanFind{
+        width:100% !important;
+        height: 60% !important;
+    }
+    .label_tipo{
+        width:80% !important;
+    } 
+    .label_tipo_1{
+        width:70% !important;
+    } 
+    .hidden_button{
+        display: none; 
+    }
+    .add_btn{
+        float:right !important;
+        margin-right:5% !important;
+    }
+    .observ{
+        width:65% !important;
+        margin-right: 0px !important;
+    }
+    .inputLend{
+        width:100% !important;  
+    }
+    .content_lend{
+        margin-left:17% !important;
+    }
+    .content_add{
+        margin-left:10% !important;
+    }
+    .crudContent{
+        width:100%;
+        margin-bottom:10% !important;
+    }
+    .navContent{
+        height: 100vh !important;
+    }
+}
 </style>
 
 <body class="body">
